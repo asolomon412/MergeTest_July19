476ce48b577901ac54d5f20187c71a952056f37d
Antonella Solomon <antonellasolomon@gmail.com>
2019-08-01 10:50:17 -0400

    initial commit

---
+++ src/co/gc/MergeTesting.java
@@ -0,0 +1,11 @@
+package co.gc;
+
+public class MergeTesting {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Antonella");
+
+	}
+
+}
