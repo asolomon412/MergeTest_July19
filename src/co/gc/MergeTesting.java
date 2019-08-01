206505078ef6cb46f0515ee3e8f846ff56d10b4f
Antonella Solomon <antonellasolomon@gmail.com>
2019-08-01 11:08:57 -0400

    added a new line to the top

--- src/co/gc/MergeTesting.java
+++ src/co/gc/MergeTesting.java
@@ -3,6 +3,7 @@ package co.gc;
 public class MergeTesting {
 
 	public static void main(String[] args) {
+		System.out.println("Merging is fun, when you know what you're doing");
 		System.out.println("Hello my name is Maurice");
 		System.out.println("Hello World");
 		System.out.println("My name is Antonella");
